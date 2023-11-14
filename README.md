e3265d3ffad32f1f627f5d3f7f7cd2f87adad9e3
Rafael Estévez Toledo <rafaelestevez99@gmail.com>
2023-11-14 21:35:33 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CalculadoraFXML
+ Calculadora en JavaFXML
---
+++ src/main/java/calculadorafxml/Calculadora.java
@@ -0,0 +1,30 @@
+package calculadorafxml;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Calculadora extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+        
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
